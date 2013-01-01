165adb7df1937cb4b284aadad52fb065cf676f7f
Yves Senn <yves.senn@gmail.com>
2013-01-01 18:12:50 +0100

    unit tests no longer depend on mtime resoultion

--- test/unit/application_watcher_test.rb
+++ test/unit/application_watcher_test.rb
@@ -1,5 +1,6 @@
 require "helper"
 require "fileutils"
+require "active_support/core_ext/numeric/time"
 require "spring/application_watcher"
 
 class ApplicationWatcherTest < Test::Unit::TestCase
@@ -12,21 +13,21 @@ class ApplicationWatcherTest < Test::Unit::TestCase
     FileUtils.rm_r(@dir)
   end
 
-  def touch(file)
-    sleep 0.01
-    File.write(file, "omg")
+  def touch(file, mtime = nil)
+    options = {}
+    options[:mtime] = mtime if mtime
+    FileUtils.touch(file, options)
   end
 
   def test_file_mtime
     file = "#{@dir}/omg"
-    touch file
+    touch file, Time.now - 2.seconds
 
     watcher = Spring::ApplicationWatcher.new
     watcher.add_files [file]
 
     assert !watcher.stale?
-
-    touch file
+    touch file, Time.now
     assert watcher.stale?
   end
 
@@ -39,16 +40,16 @@ class ApplicationWatcherTest < Test::Unit::TestCase
 
     assert !watcher.stale?
 
-    touch "#{@dir}/1/foo"
+    touch "#{@dir}/1/foo", Time.now - 1.minute
     assert !watcher.stale?
 
-    touch "#{@dir}/1/foo.rb"
+    touch "#{@dir}/1/foo.rb", 2.seconds
     assert watcher.stale?
 
     watcher.reset
     assert !watcher.stale?
 
-    touch "#{@dir}/2/foo"
+    touch "#{@dir}/2/foo", Time.now
     assert watcher.stale?
   end
 end
