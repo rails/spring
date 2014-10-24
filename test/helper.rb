281686fd52691e8bb21c91f56f3d804e7f00ad2d
Cezary Baginski <cezary@chronomantic.net>
2014-10-24 18:46:30 +0200

    use celluloid/test and show celluloid warnings

--- test/helper.rb
+++ test/helper.rb
@@ -4,4 +4,7 @@ require "bundler/setup"
 require "active_support/test_case"
 require "minitest/autorun"
 
+require "celluloid/test"
+Celluloid.logger.level = Logger::WARN
+
 TEST_ROOT = File.expand_path('..', __FILE__)
