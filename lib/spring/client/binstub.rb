e31716d8654520cde26d61d271e6eca1d5c02192
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2016-02-26 13:26:48 +0100

    Set GEM_PATH as string as it does not accept arrays in RubyGems 2.6.0+

--- lib/spring/client/binstub.rb
+++ lib/spring/client/binstub.rb
@@ -38,7 +38,7 @@ unless defined?(Spring)
   require 'bundler'
 
   if (match = Bundler.default_lockfile.read.match(/^GEM$.*?^    (?:  )*spring \((.*?)\)$.*?^$/m))
-    Gem.paths = { 'GEM_PATH' => [Bundler.bundle_path.to_s, *Gem.path].uniq }
+    Gem.paths = { 'GEM_PATH' => [Bundler.bundle_path.to_s, *Gem.path].uniq.join(Gem.path_separator) }
     gem 'spring', match[1]
     require 'spring/binstub'
   end
