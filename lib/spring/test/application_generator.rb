b482a4ae663c84de710f55aeaa384d7404b28dbf
Rafael Mendonça França <rafaelmfranca@gmail.com>
2019-05-07 17:28:04 -0400

    Only keep 1 sqlite3 entry in the Gemfile

--- lib/spring/test/application_generator.rb
+++ lib/spring/test/application_generator.rb
@@ -54,16 +54,20 @@ module Spring
 
         append_to_file(application.gemfile, "gem 'spring', '#{Spring::VERSION}'")
 
-        if @version.to_s < '5.2'
-          append_to_file(application.gemfile, "gem 'sqlite3', '< 1.4'")
-        end
-
         rewrite_file(application.gemfile) do |c|
           c.sub!("https://rubygems.org", "http://rubygems.org")
           c.gsub!(/(gem '(byebug|web-console|sdoc|jbuilder)')/, "# \\1")
+
+          if @version.to_s < '5.2'
+            c.gsub!(/(gem 'sqlite3')/, "# \\1")
+          end
+
           c
         end
 
+        if @version.to_s < '5.2'
+          append_to_file(application.gemfile, "gem 'sqlite3', '< 1.4'")
+        end
 
         if application.path("bin").exist?
           FileUtils.cp_r(application.path("bin"), application.path("bin_original"))
