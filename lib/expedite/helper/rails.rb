b609efd5e0cb03901e9d7a476aa2b87d4e7eb3f1
Bing-Chang Lai <johnny.lai@me.com>
2022-12-15 21:58:11 -0800

    rails_environment agent no longer loads the rackup. It is now just the rails environment.

--- lib/expedite/helper/rails.rb
+++ lib/expedite/helper/rails.rb
@@ -1,14 +1,8 @@
 
 Expedite.define do
+  # Agent that rails environment loaded
   agent :rails_environment do
     app_root = Dir.pwd
-
-    require "#{app_root}/config/boot.rb"
-
-    require "rack"
-    rackup_file = "#{app_root}/config.ru"
-    Rack::Builder.load_file(rackup_file)
-
-    Rails.application.eager_load!
+    require "#{app_root}/config/environment.rb"
   end
 end
